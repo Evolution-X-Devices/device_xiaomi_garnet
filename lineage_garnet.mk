2129201a87e6d44388d3b4a64ccbc91124dbbf75
Omar Mohamed <omarcoptan9@gmail.com>
2024-04-28 16:34:11 +0000

    garnet: Initialize for EvolutionX 11.x
    Change-Id: I110934e5e35eacfdf2ded09df358ebc7cb7c0a9c
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -9,11 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
+
+# EvolutionX Config
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_DISABLE_EPPE := true
+TARGET_INCLUDE_ACCORD := false
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_NAME := lineage_garnet
 PRODUCT_DEVICE := garnet
