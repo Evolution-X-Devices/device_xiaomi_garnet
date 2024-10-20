05d259608042a6a3f0eb861624679e8c980e74ca
Arian <arian.kulmer@web.de>
2024-10-20 08:54:49 +0000

    garnet: Patch libcamximageformatutils to get rid of ndk_platform qti display config dependency
    Change-Id: I9094e549133f9cdb0db768dccc17df29f55d0860
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -97,6 +97,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i -E '/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d' "${2}"
             ;;
+        vendor/lib64/libcamximageformatutils.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
+            ;;
         vendor/lib64/libgrpc++_unsecure_prebuilt.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname "libgrpc++_unsecure_prebuilt.so" "${2}"
