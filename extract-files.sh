69b67fb815f9aa8ba5035492deb2410d13d0628a
Nick Reuter <nreuter85@gmail.com>
2024-12-10 20:32:13 -0600

    garnet: Address missing libbinder symbols in libwfdnative
    _ZN7android6binder12atrace_beginEmPKc
    _ZN7android6binder10atrace_endEm
    
    Change-Id: I942d51721461dbbf9c8d0f2f32128529bb7480c3
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -68,6 +68,7 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             [ "$2" = "" ] && return 0
             grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
+            grep -q "libbinder_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
             ;;
         system_ext/lib64/libwfdservice.so)
             [ "$2" = "" ] && return 0
