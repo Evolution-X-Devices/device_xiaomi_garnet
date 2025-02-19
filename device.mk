9241c0fb8290b8f961b784b5cc977057be22a1f1
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    garnet: Switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -133,11 +133,8 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti:64 \
-    android.hardware.boot@1.2-impl-qti.recovery:64 \
-    android.hardware.boot@1.2-service \
-    bootctrl.garnet:64 \
-    bootctrl.garnet.recovery:64
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 # Camera
 PRODUCT_PACKAGES += \
