96e265520059b1b8def6ef67233fb64aab53ebd2
Bruno Martins <bgcngm@gmail.com>
2025-03-13 21:37:39 +0000

    garnet: Shim camera libs
    Change-Id: Id4bec2bc8bf5da014a69e1953bdb36bc85d11f1e
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -83,8 +83,17 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('.+dolby.+\n', ''),
     'vendor/etc/media_codecs_parrot_v0.xml': blob_fixup()
         .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
+    (
+        'vendor/lib64/hw/camera.qcom.so',
+        'vendor/lib64/hw/com.qti.chi.override.so'
+    ): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
+    'vendor/lib64/libcamxcommonutils.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/libcamximageformatutils.so': blob_fixup()
         .replace_needed('vendor.qti.hardware.display.config-V2-ndk_platform.so', 'vendor.qti.hardware.display.config-V2-ndk.so'),
+    'vendor/lib64/libmialgoengine.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/libQnnDspV65CalculatorStub.so': blob_fixup()
         .add_needed('liblog.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
