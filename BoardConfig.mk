fca4dc22b7981fba5b6a4c2da63e47820bf310aa
Arian <arian.kulmer@web.de>
2025-05-06 14:23:33 +0530

    garnet: sepolicy: Import initial sepolicy for Xiaomi parts
    * It was missing before
    
    Change-Id: I4da72f827a17ef95091ccd8b35f1c90d51e8905a
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,6 +196,8 @@ include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
