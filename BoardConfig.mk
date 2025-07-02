b5ce395dbad801987a2a0debc153741efa449bfa
AdarshGrewal <adarshgrewal@gmail.com>
2025-07-02 12:46:03 -0300

    garnet: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -238,3 +238,6 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Vendor
 include vendor/xiaomi/garnet/BoardConfigVendor.mk
+
+# Vendor MiuiCamera
+-include device/xiaomi/garnet-miuicamera/BoardConfig.mk
--- device.mk
+++ device.mk
@@ -463,3 +463,6 @@ PRODUCT_COPY_FILES += \
 
 # Vendor
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
+
+# Vendor MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/garnet-miuicamera/device.mk)
