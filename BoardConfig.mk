35cf3b298749cc2ee1211739f9ef2206b2cfbc71
ccfries <cfries@google.com>
2025-10-12 15:34:07 -0300

    garnet: Set VENDOR_SECURITY_PATCH To PLATFORM
    Set the VENDOR_SECURITY_PATCH level to match the platform security patch level.
    These are functionally the same.
    
    Change-Id: Id7fab7d46392b31848cda40b9bbd1c4b1dadf577
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,7 +201,7 @@ TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/props/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
 
 # Vendor security patch
-VENDOR_SECURITY_PATCH := 2025-12-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
