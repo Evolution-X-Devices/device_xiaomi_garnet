0ef3a91f7f4ebc1caae7c196835a2589f5288ae0
Omar Mohamed <omarcoptan9@gmail.com>
2026-05-01 15:57:52 +0000

    Revert "garnet: Call extra.config to be merged as device defconfig"
    This reverts commit 7fee3c54d27685dcec5b9e422bb27c5c76df38cf.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,8 +100,7 @@ TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/parrot_GKI.config \
     vendor/garnet_GKI.config \
-    vendor/debugfs.config \
-    vendor/extra.config
+    vendor/debugfs.config
 
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
