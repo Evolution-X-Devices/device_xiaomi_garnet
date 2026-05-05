7efcab3e5687cb1cdfcebdc7eb3ccb1fb5a9855c
AdarshGrewal <adarshgrewal@gmail.com>
2026-05-05 13:05:25 +0530

    garnet: Update from OS3.0.5.0.WNRMIXM
    Change-Id: I90808f2bb7a9329fe3f2b807cfacc972da2f673f
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -41,6 +41,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/etc/init/qspa_system.rc': blob_fixup()
+        .regex_replace(r'\$\{ro\.boot\.vendor\.qspa:-default\}', 'default'),
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
         .replace_needed(
             'libprotobuf-cpp-full.so',
@@ -124,13 +126,14 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libQnnDspV65CalculatorStub.so': blob_fixup()
         .add_needed('liblog.so'),
     (
-        'odm/lib64/libMiVideoFilter.so',
+        'odm/lib64/libMiPhotoFilter.so',
         'vendor/lib64/libalhLDC.so',
         'vendor/lib64/libalLDC.so',
         'vendor/lib64/libTrueSight.so'
     ): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_isSupported')
         .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_lockPlanes')
         .clear_symbol_version('AHardwareBuffer_release')
